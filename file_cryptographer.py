936d44bd7c340674e10b0c286e7dff96112e3c7e
glbessa <gabrielleitebessa@gmail.com>
2020-10-09 10:20:14 -0300

    introduced an option to implement initializator vector

--- file_cryptographer.py
+++ file_cryptographer.py
@@ -7,7 +7,8 @@ def arguments():
     ap.add_argument('-f', '--file', required=True, help='the file that will be encrypt or decrypt', nargs='+')
     ap.add_argument('-o', '--operation', required=True, help='what I will do for you?', choices=['decrypt', 'encrypt'])
     ap.add_argument('-k', '--key', help='the key that the program will use to encrypt or decrypt the file')
-    ap.add_argument('-ks', '--key-size', choices=['128', '192', '256'], help='the size of the key that you want')
+    ap.add_argument('-iv', '--initializator-vector', help='the initializator vector of the cipher block')
+    ap.add_argument('-ks', '--key-size', default='256', choices=['128', '192', '256'], help='the size of the key that you want')
     return vars(ap.parse_args())
 
 def str_2_hex(string_:str):
@@ -22,20 +23,29 @@ def data_corrector(data:bytes, block_size:int):
     return data.encode()
 
 def main(args):
-    cipher, key = create_cipher(args['key'], args['key_size'])
+    cipher, key, iv = create_cipher(args['key'], args['initializator_vector'], int(args['key_size']))
     if args['operation'] == 'encrypt':
-        map(encrypt_file, (args['file'], cipher))
+        for file_ in args['file']:
+            encrypt_file(file_, cipher)
     else:
-        map(decrypt_file, (args['file'], cipher))
+        for file_ in args['file']:
+            decrypt_file(file_, cipher)
     print(hexdigest(key))
+    print(hexdigest(iv))
 
 
-def create_cipher(key, key_size=256, mode=modes.CBC, iv=os.urandom(16)):
-    if key == False:
-        key = os.urandom((key_size/8))
+def create_cipher(key, iv, key_size=256, mode=modes.CBC):
+    if key == None:
+        key = os.urandom(int(key_size/8))
     else:
         key = str_2_hex(key)
-    return Cipher(algorithms.AES(key), mode(iv)), key
+    if iv != None:
+        iv = str_2_hex(iv)
+        if len(iv) != 16:
+            raise Exception
+    else:
+        iv = os.urandom(16)
+    return Cipher(algorithms.AES(key), mode(iv)), key, iv
 
 def decrypt_file(file_, cipher_):
     data = None
@@ -52,7 +62,8 @@ def encrypt_file(file_, cipher_):
     with open(file_, 'rb') as reader:
         data = reader.read()
 
-    encryptor_ = cipher_.encrypter()
+    data = data_corrector(data, 16)
+    encryptor_ = cipher_.encryptor()
     data = encryptor_.update(data) + encryptor_.finalize()
     with open(file_, 'wb') as writer:
         writer.write(data)
